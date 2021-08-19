30e401177e6cbab8306d52ad748d6c886ce42cd0
Alexander Zabiralov <zabiralov@gmail.com>
2021-08-19 21:34:25 +0200

    Add exit if start.sh failed

--- start.sh
+++ start.sh
@@ -4,32 +4,28 @@
 echo -n "Create mail aliases ... "
 if /usr/sbin/postalias -c /etc/mailer lmdb:/etc/mailer/aliases
 then echo OK
-else echo FAIL
+else exit 1
 fi
 
 
 echo -n "Create sasldb2 database ... "
 if echo -n 'AHmOi2CCVtchGeG' | /usr/sbin/saslpasswd2 -f /etc/sasl2/sasldb2 -u "$DKIM_DOMAIN" -p "$RELAY_USER"
 then echo OK
-else echo FAIL
+else exit 1
 fi
 
 
 echo -n "Set permissions for $DKIM_KEYFILE ... "
 if chmod 400 $DKIM_KEYFILE
 then echo OK
-else echo FAIL
+else exit 1
 fi
 
 
 echo -n "Start OpenDKIM ... "
 if /usr/sbin/opendkim -d "$DKIM_DOMAIN" -k "$DKIM_KEYFILE" -s "$DKIM_SELECTOR" -x /etc/mailer/opendkim.conf
 then echo OK
-else echo FAIL
+else exit 1
 fi
 
-
 exec /usr/sbin/postfix -c /etc/mailer start-fg
-
-
-# exec /bin/sleep inf
