ef5af991ebfa11882793017efb6ea1e4c715080b
Alexander Zabiralov <zabiralov@gmail.com>
2021-08-19 16:52:48 +0300

    v21.231 (#2)
    * Add sasl-auth, netcat, mailx and various -doc packages
    * Add mail aliases
    * Add SMTP auth via cyrus-sasl framework
    * Disable syslogd, send logs to stderr
    * Split to minimal and with docs build

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
-docker-mailer
----
+# docker-mailer #
 
+#### DESCRIPTION ####
 Alpine-based image with Postfix and OpenDKIM
 
-Run `make build` for create actual image
 
+#### BUILD AND RUN ####
+Run `make build` for create actual image (minimal)
+
+Run `make buildf` for create actual image (with doc packages installed)
+
+Run `make up` for start via docker-compose
+
+Run `make down` for stop
+
+#### ENVIRONMENT ####
+```
+    DKIM_DOMAIN: example.com
+    DKIM_KEYFILE: /etc/mailer/default.private
+    DKIM_SELECTOR: default
+    RELAY_USER: relay
+    RELAY_PASS: securepass
+```
--- start.sh
+++ start.sh
@@ -1,18 +1,35 @@
 #!/usr/bin/env sh
 
-# Start busybox syslog with redirect to stdout
-echo "Start syslogd ..."
-/sbin/syslogd -s 0 -O -
 
-# Start opendkim daemon
-echo "Start opendkim ..."
-/usr/sbin/opendkim -d "$DKIM_DOMAIN" -k "$DKIM_KEYFILE" -s "$DKIM_SELECTOR" -x /etc/mailer/opendkim.conf
+echo -n "Create mail aliases ... "
+if /usr/sbin/postalias -c /etc/mailer lmdb:/etc/mailer/aliases
+then echo OK
+else echo FAIL
+fi
 
-# Wait for opendkim
-/bin/sleep 2
 
-# Start Postfix MTA
-/usr/sbin/postfix -c /etc/mailer start
+echo -n "Create sasldb2 database ... "
+if echo -n 'AHmOi2CCVtchGeG' | /usr/sbin/saslpasswd2 -f /etc/sasl2/sasldb2 -u "$DKIM_DOMAIN" -p "$RELAY_USER"
+then echo OK
+else echo FAIL
+fi
 
-# Hold script run
-exec /bin/sleep inf
+
+echo -n "Set permissions for $DKIM_KEYFILE ... "
+if chmod 400 $DKIM_KEYFILE
+then echo OK
+else echo FAIL
+fi
+
+
+echo -n "Start OpenDKIM ... "
+if /usr/sbin/opendkim -d "$DKIM_DOMAIN" -k "$DKIM_KEYFILE" -s "$DKIM_SELECTOR" -x /etc/mailer/opendkim.conf
+then echo OK
+else echo FAIL
+fi
+
+
+exec /usr/sbin/postfix -c /etc/mailer start-fg
+
+
+# exec /bin/sleep inf
